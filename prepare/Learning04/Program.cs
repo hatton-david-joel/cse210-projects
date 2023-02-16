5fee7762ca7038adfca033e3cb916e053d4280d4
th3traveler <thetraveler@charonsobols.com>
2023-02-15 21:40:41 -0500

    Finished assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment firstAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(firstAssignment.GetSummary());
+
+        MathAssignment secondAssignment = new MathAssignment("David Hatton", "Division", "7.3", "8-19");
+        Console.WriteLine(secondAssignment.GetSummary());
+        Console.WriteLine(secondAssignment.GetHomeworkList());
+
+        WritingAssignment thirdAssignment = new WritingAssignment("Neal Walters", "Music", "Reading Music");
+        Console.WriteLine(thirdAssignment.GetSummary());
+        Console.WriteLine(thirdAssignment.GetWritingInformation());
     }
 }
